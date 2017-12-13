aa28fc54663f32e65a599fc4065a4f2f4dda6c8a
fel111 <felix.goupil@free.fr>
2017-12-13 18:21:11 +0100

    tp3 ajout contraintes vertices

--- tp3.ml
+++ tp3.ml
@@ -22,9 +22,9 @@ let fprint = fun filename t x y -> (* printing for GNUplot *)
 let solve = fun size t ->
 	let x = Array.map (fun ti -> Fd.interval 0 (size-ti)) t in
 	let y = Array.map (fun ti -> Fd.interval 0 (size-ti)) t in
-	let n = Array.length(t) in	
+	let n = Array.length t in	
 
-	(*let w = Array.mapi (fun i ti -> Interval.is_member ( x.(i), *)
+	
 	for i=0 to n-1 do
 		for j=i+1 to n-1 do
 			Cstr.post ((fd2e x.(i) +~ i2e t.(i) <=~ fd2e x.(j)) ||~~
@@ -33,15 +33,25 @@ let solve = fun size t ->
  						(fd2e y.(j) +~ i2e t.(j) <=~ fd2e y.(i)))
 		done
 	done;
+	for v=0 to size-1 do
+		let w = Array.mapi (fun i ti -> Interval.is_member x.(i) (v-ti+1) v) t in
+		let z = Array.mapi (fun i ti -> Interval.is_member y.(i) (v-ti+1) v) t in
+		Cstr.post (Arith.scalprod_fd t w =~ i2e size);
+		Cstr.post (Arith.scalprod_fd t z =~ i2e size)
+	done;
 
-	let goal = Goals.Array.labeling x &&~ Goals.Array.labeling y in
-  	if Goals.solve goal then
+	(*let goal = Goals.Array.labeling x &&~ Goals.Array.labeling y in*)
+	let minmin = Goals.Array.choose_index (fun v1 v2 -> Fd.min v1 < Fd.min v2) in
+	let goal = Goals.Array.forall ~select:minmin Goals.assign x &&~ Goals.Array.forall ~select:minmin Goals.assign y in
+	let print_bt = fun bt -> Printf.printf "\r%d%!\n" bt in
+  	if Goals.solve ~control:print_bt goal then
     	fprint "solutions" t x y
   	else Printf.printf "No solution found\n"
 
 let () =
-	let idx = int_of_string Sys.argv.(1) in
+	let idx = int_of_string Sys.argv.(1) in	
 	let (size, t) = data.(idx) in
+	
 	solve size t
 
 	
